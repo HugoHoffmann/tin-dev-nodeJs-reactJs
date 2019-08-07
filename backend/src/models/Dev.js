1c160a811dbda114c5ea2fdf5720bf7413159281
Hugo <hugo.hoffmann@nxfacil.com.br>
2019-08-07 13:22:41 -0300

    update Dev Model

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -14,6 +14,16 @@ const DevSchema = new Schema({
         type: String,
         required: true,
     },
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+
+    }],
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Dev',
+
+    }],
 }, {
     timestamps: true,
 });
