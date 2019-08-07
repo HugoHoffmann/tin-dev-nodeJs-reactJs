b26b37fa9f322fcf460751e9c3522df766c2be61
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-08-07 20:46:01 -0300

    Add Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = ('./controllers/LikeController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({ ok: true });
 })
-routes.post('/devs', DevController.store)
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
-mongoose.connect('mongodb+srv://hugo:hugoho123@cluster0-df8xh.mongodb.net/test?retryWrites=true&w=majorityy', { useNewUrlParser: true });
+// mongoose.connect('mongodb+srv://hugo:hugoho123@cluster0-df8xh.mongodb.net/test?retryWrites=true&w=majorityy', { useNewUrlParser: true });
 
 
 server.use(express.json)
