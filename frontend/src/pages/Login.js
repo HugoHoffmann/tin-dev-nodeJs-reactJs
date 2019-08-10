b17f27d9f8dea7c4ce269a0c5e45d831e994950c
Hugo Hoffmann <hugohoffmann04@gmail.com>
2019-08-10 20:50:10 -0300

    Use routes and axios

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../services/api';
 import logo from '../assets/logo.svg';
 import './Login.css'
 
-export default function Login() {
+export default function Login({ hitory }) {
+    const [username, setUsername] = useState('');
+    async function handleSubmit(e){
+        e.preventDefault();
+        const response = await api.post('/devs', {
+            username,
+        });
+        
+        console.log(response);
+        hitory.push('/main');
+
+    }
     return (
         <div className="login-container">
-            <form >
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev"/>
-                <input placeholder="Informe o seu usuário do GitHub"/>
+                <input 
+                    placeholder="Informe o seu usuário do GitHub"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit" >Login</button>
             </form>
         </div>
